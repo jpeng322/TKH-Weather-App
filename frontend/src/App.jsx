0fdf0ab26c97b4084e12fd80b659a774e76f67a4
Jacky <jackypengsmail@gmail.com>
2023-06-24 02:33:59 -0400

    Create daily and 3 hr page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import WeatherApp from "./WeatherApp";
 import { getData } from "../api";
 function App() {
-  const [weatherData, setWeatherData] = useState();
-
-  const [fiveDayData, setFiveDayData] = useState()
 
 
   
@@ -19,97 +16,8 @@ function App() {
     },
   ]);
 
-  console.log(weatherData);
-
-  const KelvinToFah = (temp) => {
-    const fah = Math.ceil(((temp - 273.15) * 9) / 5 + 32);
-
-    return fah;
-  };
-
-  const UtcToUsa = (time) => {
-    // var utcTime = new Date("2023-06-24 03:00:00");
-    const utcTime = new Date(time);
-    let usaTime = utcTime.toLocaleString("en-US", {
-      timeZone: "America/New_York",
-    });
-    // console.log(usaTime.split(" ")[1].replace(/[0:]/g, ''))
-    usaTime =
-      usaTime.split(" ")[0] +
-      " " +
-      usaTime.split(" ")[1].replace(/[0:]/g, "") +
-      " " +
-      usaTime.split(" ")[2];
-
-    // console.log(date, noZeros, usaTime.split(" ")[2])
-    // usaTime = usaTime.replace(/[0:]/g, '');
-    // const newUsa= usaTime.replaceAll("0", " ").replaceAll(":"," ")
-    return usaTime;
-  };
-
-  
-  const createData = async () => {
-    const arrayOfDaysData = [];
-    let counter = 0;
-    let dataArray = [];
-    
-    while (counter < weatherData.list.length) {
-      if (weatherData.list[counter].dt_txt.split(" ")[1] === "00:00:00") {
-        arrayOfDaysData.push(dataArray);
-        dataArray = [];
-      }
-      dataArray.push(weatherData.list[counter]);
-        counter++;
-      console.log(dataArray);
-    }
-
-    setFiveDayData(arrayOfDaysData);
-  };
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios({
-          method: "get",
-          url: `https://api.openweathermap.org/data/2.5/forecast?lat=40.71&lon=-74.00&&appid=${
-            import.meta.env.VITE_WEATHER_KEY
-          }`,
-        });
-
-        if (response) {
-          const data = response.data;
-          console.log(data);
-          setWeatherData(data);
-          createData()
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    getData();
 
 
-    
-  }, []);
-
-  // const createData = () => {
-  //   const arrayOfDaysData = [];
-  //   let counter = 0;
-  //   let dataArray = [];
-  //   while (counter < weatherData.list.length) {
-  //     if (weatherData.list[counter].dt_txt.split(" ")[1] === "00:00:00") {
-  //       arrayOfDaysData.push(dataArray);
-  //       dataArray = [];
-  //     }
-  //     dataArray.push(weatherData.list[counter]);
-  //       counter++;
-  //     console.log(dataArray);
-  //   }
-
-  //   return arrayOfDaysData;
-  // };
-
-  // console.log(createData());
   return (
     <RouterProvider router={router} />
    
--- frontend/src/WeatherApp.jsx
+++ frontend/src/WeatherApp.jsx
@@ -8,8 +8,7 @@ const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState(loaderWeatherData);
 
   const [fiveDayData, setFiveDayData] = useState();
-
-  console.log(weatherData);
+  const [toggleDaily, setToggleDaily] = useState();
 
   const KelvinToFah = (temp) => {
     const fah = Math.ceil(((temp - 273.15) * 9) / 5 + 32);
@@ -25,11 +24,9 @@ const WeatherApp = () => {
     });
     // console.log(usaTime.split(" ")[1].replace(/[0:]/g, ''))
     usaTime =
-      usaTime.split(" ")[0] +
-      " " +
-      usaTime.split(" ")[1].replace(/[0:]/g, "") +
-      " " +
-      usaTime.split(" ")[2];
+      //   usaTime.split(" ")[0] +
+      //   " " +
+      usaTime.split(" ")[1].replace(/[0:]/g, "") + " " + usaTime.split(" ")[2];
 
     // console.log(date, noZeros, usaTime.split(" ")[2])
     // usaTime = usaTime.replace(/[0:]/g, '');
@@ -37,32 +34,98 @@ const WeatherApp = () => {
     return usaTime;
   };
 
-  const createData = async () => {
-    const arrayOfDaysData = [];
-    let counter = 0;
-    let dataArray = [];
+  const date = new Date("2023-06-24 03:00:00");
+  let usaTime = date.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
 
-    while (counter < weatherData.list.length) {
-      if (weatherData.list[counter].dt_txt.split(" ")[1] === "00:00:00") {
-        arrayOfDaysData.push(dataArray);
-        dataArray = [];
+  function UtcToDay(time) {
+    const date = new Date(time);
+    const typeOfDay = date.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+
+    return typeOfDay;
+  }
+
+  useEffect(() => {
+    const createData = () => {
+      const arrayOfDaysData = [];
+      let counter = 0;
+      let dataArray = [];
+
+      while (counter < weatherData.list.length) {
+        if (weatherData.list[counter].dt_txt.split(" ")[1] === "00:00:00") {
+          arrayOfDaysData.push({
+            date: weatherData.list[counter - 1].dt_txt,
+            dataArray,
+          });
+          dataArray = [];
+        }
+        dataArray.push(weatherData.list[counter]);
+        counter++;
+        // console.log(counter)
       }
-      dataArray.push(weatherData.list[counter]);
-      counter++;
-    }
+      console.log(arrayOfDaysData);
 
-    console.log(arrayOfDaysData);
+      setFiveDayData(arrayOfDaysData);
+    };
+
+    createData();
+  }, []);
+
+  console.log(fiveDayData);
+
+  //   const startingPrice = fiveDayData[0].dataArray.reduce(function (prev, curr) {
+  //     return prev.main.temp_max < curr.main.temp_min ? prev.main.temp_max : curr.main.temp_min;
+  //   });
+
+  //   console.log(
+  //     fiveDayData[0].dataArray.reduce(function (prev, curr) {
+  //       return prev.main.temp_max < curr.main.temp_min ? prev : curr;
+  //     })
+  //   );
+
+  const getHighOrLowTemp = (dataArray, type = "high") => {
+    const tempObj = dataArray.dataArray.reduce(function (prev, curr) {
+      if (type === "low") {
+        return prev.main.temp_max < curr.main.temp_min ? prev : curr;
+      } else if (type === "high") {
+        return prev.main.temp_max > curr.main.temp_min ? prev : curr;
+      } else {
+        return null;
+      }
+    });
+
+    const temp = KelvinToFah(tempObj.main.temp_max);
+    return temp;
   };
 
-  createData();
-
+  // console.log(starting)
   return (
     <>
-      {weatherData && (
-        <div className="weather-container">
-          <div>{weatherData.city.name}</div>
-
-          <div className="weather-cards-container">
+      <div className="weather-container">
+        <header>
+          <h1>{weatherData.city.name} 5-Day Forecast</h1>
+          <div className="button-container">
+            <button
+              className={toggleDaily ? "active" : ""}
+              onClick={() => setToggleDaily(true)}
+            >
+              Daily
+            </button>
+            <button
+              className={toggleDaily ? "" : "active"}
+              onClick={() => setToggleDaily(false)}
+            >
+              3 Hours
+            </button>
+          </div>
+        </header>
+        {/* <div className="weather-cards-container">
             {weatherData.list.map((day, index) => {
               return (
                 <div key={index} className="weather-card">
@@ -80,9 +143,71 @@ const WeatherApp = () => {
                 </div>
               );
             })}
-          </div>
-        </div>
-      )}
+          </div> */}
+        {toggleDaily
+          ? fiveDayData && (
+              <div className="weather-daily-cards-container">
+                {fiveDayData.map((dayArray) => {
+                  return (
+                    <div className="weather-daily">
+                      <h3> {UtcToDay(dayArray.date)} </h3>
+                      <div className="weather-daily-info">
+                        <div className="weather-card">
+                          <div className="weather-temp">
+                            {KelvinToFah(dayArray.dataArray[3].main.temp)} °F
+                          </div>
+                          <div className="weather-daily-high-low">
+                            <div>
+                              High: {getHighOrLowTemp(dayArray, "high")}
+                            </div>
+                            <div>Low: {getHighOrLowTemp(dayArray, "low")}</div>
+                          </div>
+                          <img
+                            src={`https://openweathermap.org/img/wn/${dayArray.dataArray[0].weather[0].icon}@2x.png`}
+                            alt="weather_icon"
+                          />
+                          <div className="weather-description">
+                            {dayArray.dataArray[3].weather[0].description}
+                          </div>
+                          <div>{UtcToUsa(dayArray.dataArray[3].dt_txt)}</div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            )
+          : fiveDayData && (
+              <div className="weather-cards-container">
+                {fiveDayData.map((dayArray) => {
+                  return (
+                    <div className="weather-day">
+                      <h3> {UtcToDay(dayArray.date)} </h3>
+                      <div className="weather-day-info">
+                        {dayArray.dataArray.map((day, index) => {
+                          return (
+                            <div key={index} className="weather-card">
+                              <div className="weather-temp">
+                                {KelvinToFah(day.main.temp)} °F
+                              </div>
+                              <img
+                                src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+                                alt="weather_icon"
+                              />
+                              <div className="weather-description">
+                                {day.weather[0].description}
+                              </div>
+                              <div>{UtcToUsa(day.dt_txt)}</div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+      </div>
     </>
   );
 };
